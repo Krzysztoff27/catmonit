394fcab6dbdce6574af6f26db4358fac99ca722a
NPrze <natalia.przemylska@gmail.com>
2025-05-02 10:28:54 +0300

    [feature] connection and basic controller

---
+++ webapi/Controllers/user/LoginController.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using webapi.Models;
+namespace webapi.Controllers.api_main
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var data = new { Message = "Hello, JSON!", Time = DateTime.UtcNow };
+            return Json(data);
+        }
+        [HttpPost]
+        public JsonResult Post([FromBody] UserModel user)
+        {
+            var data = new { Message = "Hello, " + user.username };
+            return Json(data);
+        }
+    }
+}
--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,3 +1,5 @@
+using MySql.Data.MySqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
@@ -20,5 +22,29 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "json",
+    pattern: "{controller=Test}/{action=Get}/{id?}");
+
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+string connectionString = "Server=webapi-mysql-1;Database=catmonit;User=root;Password=mysecretpassword;";
+
+using (MySqlConnection conn = new MySqlConnection(connectionString))
+{
+    conn.Open();
+
+    string query = "SELECT * FROM users";
+    MySqlCommand cmd = new MySqlCommand(query, conn);
+
+    using (MySqlDataReader reader = cmd.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            string msg = reader["username"].ToString();
+            Console.WriteLine(msg);
+        }
+    }
+}
 
 app.Run();
