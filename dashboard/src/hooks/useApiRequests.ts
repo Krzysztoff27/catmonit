16546993eea61c84f15ad0e732442d36533f310f
TeKayy <th3tk.idk@gmail.com>
2025-05-06 10:17:05 +0300

    Implemented api related hooks

---
+++ dashboard/src/hooks/useApiRequests.ts
@@ -0,0 +1,91 @@
+import useErrorHandler from "./useErrorHandler.ts";
+import { ErrorCallbackFunction, useApiReturn } from "../types/hooks.types.ts";
+import urlConfig from "../config/url.config.ts";
+import useAuth from "./useAuth.ts";
+
+const responseOnNoResponse = new Response(`{"detail": No response from server}`, {
+    status: 503,
+    headers: { "Content-Type": "text/plain" },
+});
+
+const handleFetch = async (
+    URL: string,
+    options: RequestInit = {},
+    errorCallback: (response: Response, body: { [key: string]: any }) => void,
+    tryRefreshingTokens: () => Promise<string | null>
+) => {
+    const fetchData = async () => await fetch(URL, options).catch(() => responseOnNoResponse);
+
+    let response = await fetchData();
+
+    if (`${response.status}` === "401") {
+        const token = await tryRefreshingTokens();
+        if (token) {
+            options.headers = {
+                ...options.headers,
+                Authorization: `Bearer ${token}`,
+            };
+            response = await fetchData();
+        }
+    }
+
+    // handle no content reponses
+    let json = {};
+
+    try {
+        const text = await response.text();
+        json = text ? JSON.parse(text) : {};
+    } catch (err) {
+        console.error(`Couldn't parse request response. URL=${URL}`, err);
+    }
+
+    if (!response.ok) return errorCallback(response, json);
+    return json;
+};
+
+export const useApiRequests = () => {
+    const API_URL: string = urlConfig.api_requests;
+    const { parseAndHandleError } = useErrorHandler();
+    const { authOptions, refreshOptions, setAccessToken, setRefreshToken } = useAuth();
+
+    const normalizePath = (path = "") => (path.startsWith("/") ? path : `/${path}`);
+
+    const getPath = (path: string): string => (API_URL ? `${API_URL}${normalizePath(path)}` : "");
+
+    const refreshTokens = async () => {
+        return fetch(getPath("refresh"), refreshOptions)
+            .then(res => res.ok && res.json())
+            .then(json => {
+                setAccessToken(json?.access_token);
+                setRefreshToken(json?.refresh_token);
+                return json?.access_token;
+            })
+            .catch(err => {
+                console.error("Error occured while parsing response body during token refresh.\n", err);
+                return null;
+            });
+    };
+
+    const sendRequest = async (
+        path: string,
+        method: string = "GET",
+        options: RequestInit = {},
+        body: BodyInit | undefined = undefined,
+        errorCallback: () => void
+    ): Promise<any> =>
+        await handleFetch(
+            getPath(path),
+            {
+                ...authOptions,
+                ...options,
+                method: method,
+                body: body,
+            },
+            errorCallback,
+            refreshTokens
+        );
+
+    return { getPath, sendRequest };
+};
+
+export default useApiRequests;
---
+++ dashboard/src/hooks/useAuth.ts
@@ -0,0 +1,74 @@
+import { useMemo, useCallback } from "react";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+
+/**
+ * Custom react hook providing the application with authentication support.
+ */
+
+type TokenType = "access_token" | "refresh_token";
+
+interface Tokens {
+    access_token: string | null;
+    refresh_token: string | null;
+}
+
+interface useAuthReturn {
+    tokens: Tokens;
+    authOptions: RequestInit;
+    refreshOptions: RequestInit;
+    logout: () => void;
+    clearTokens: () => void;
+    setAccessToken: (token: string | null) => void;
+    setRefreshToken: (token: string | null) => void;
+}
+
+export default function useAuth(): useAuthReturn {
+    const [cookies, setCookies] = useCookies(["access_token", "refresh_token"]);
+    const navigate = useNavigate();
+
+    const setToken = (tokenType: TokenType, newToken: string | null) => setCookies(tokenType, newToken, { path: "/" });
+
+    const setAccessToken = (token: string | null) => setToken("access_token", token);
+    const setRefreshToken = (token: string | null) => setToken("refresh_token", token);
+
+    const clearTokens = () => {
+        setAccessToken(null);
+        setRefreshToken(null);
+    };
+
+    const logout = () => {
+        clearTokens();
+        navigate("/login");
+    };
+
+    const tokens = useMemo(
+        () => ({
+            access_token: cookies.access_token,
+            refresh_token: cookies.refresh_token,
+        }),
+        [cookies.access_token, cookies.refresh_token]
+    );
+
+    const getTokenOptions = (token: string) => ({
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+    const authOptions = useMemo(() => getTokenOptions(cookies.access_token), [cookies.access_token]);
+
+    const refreshOptions = useMemo(() => getTokenOptions(cookies.refresh_token), [cookies.refresh_token]);
+
+    return {
+        setAccessToken,
+        setRefreshToken,
+        clearTokens,
+        logout,
+        tokens,
+        authOptions,
+        refreshOptions,
+    };
+}
