1b196c7580fd199c6cde1cd95cb294534152d89f
Agata <agata.stypa21@gmail.com>
2025-05-12 10:35:49 +0200

    Basic service status widget

---
+++ dashboard/src/components/ServiceStatusWidget/ServiceStatusWidget.tsx
@@ -0,0 +1,12 @@
+import { Grid } from '@mantine/core'
+import React from 'react'
+
+function ServiceStatusWidget() {
+    return (
+        <Grid>
+            <Grid.Col span={4}>Service</Grid.Col>
+        </Grid>
+    )
+}
+
+export default ServiceStatusWidget
