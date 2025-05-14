ea0d284c48fdfff0b3cc86b9764259f44f54dd32
Krzysztoff27 <krzysztof.kolasinski27@outlook.com>
2025-05-14 13:28:19 +0300

    Changed server IP to domain name.

--- telemetry-server/gRPC.telemetry.Server/webapi/Config.cs
+++ telemetry-server/gRPC.telemetry.Server/webapi/Config.cs
@@ -2,7 +2,7 @@
 {
     public const string CM_JWT_SECRET = "0b13822dac0211d86f4ca04d435025d73e3c1754e9234d63725ea7d71b53f576";
 
-    public const string CM_POSTGRES_SERVER = "172.22.0.2";
+    public const string CM_POSTGRES_SERVER = "catmonit-db";
     public const string CM_POSTGRES_USER = "user";
     public const string CM_POSTGRES_PASSWORD = "password";
     public const string CM_POSTGRES_DBNAME = "catmonit_base";
