a16f6021486bcfdd417a95f94d3ed09b1eea1d53
Krzysztof <krzysztof.kolasinski27@outlook.com>
2025-05-16 08:11:45 +0300

    Updated env variables retrieval functionality for DB credentials.

--- telemetry-server/gRPC.telemetry.Server/webapi/Config.cs
+++ telemetry-server/gRPC.telemetry.Server/webapi/Config.cs
@@ -2,9 +2,9 @@
 {
     public const string CM_JWT_SECRET = "0b13822dac0211d86f4ca04d435025d73e3c1754e9234d63725ea7d71b53f576";
 
-    public const string CM_POSTGRES_SERVER = "catmonit-db";
-    public const string CM_POSTGRES_USER = "user";
-    public const string CM_POSTGRES_PASSWORD = "password";
-    public const string CM_POSTGRES_DBNAME = "catmonit_base";
+    public const string CM_POSTGRES_SERVER = Environment.GetEnvironmentVariable("POSTGRES_SERVER") ?? "catmonit-db";
+    public const string CM_POSTGRES_USER = Environment.GetEnvironmentVariable("POSTGRES_USER") ?? "catmonit-worker";
+    public const string CM_POSTGRES_PASSWORD = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD") ?? "password";
+    public const string CM_POSTGRES_DBNAME = Environment.GetEnvironmentVariable("POSTGRES_DBNAME") ?? "catmonit_base";
     public const string CM_POSTGRES_CONNECTION_STRING = $"Host={Config.CM_POSTGRES_SERVER};Database={Config.CM_POSTGRES_DBNAME};Username={Config.CM_POSTGRES_USER};Password={Config.CM_POSTGRES_PASSWORD};";
 }
