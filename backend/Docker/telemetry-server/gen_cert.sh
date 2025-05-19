52c1375aa780399142641b8de84390a405dda9e3
Krzysztoff27 <krzysztof.kolasinski27@outlook.com>
2025-05-19 14:24:27 +0300

    Deleted old, irrelevant config files.

--- backend/Docker/telemetry-server/gen_cert.sh
+++
@@ -1,2 +0,0 @@
-openssl req -x509 -newkey rsa:4096 -keyout certs/server.key -out certs/server.crt -days 365 -nodes -subj "/CN=10.10.51.3"
-openssl pkcs12 -export -out certs/server.pfx -inkey certs/server.key -in certs/server.crt
