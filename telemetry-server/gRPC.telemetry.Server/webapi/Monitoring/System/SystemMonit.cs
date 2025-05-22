da7685a5f8d986226f6c5af53a6c3926e79c3a2f
NPrze <natalia.przemylska@gmail.com>
2025-05-22 09:22:23 +0300

    Error catch logic for improved debugging

--- telemetry-server/gRPC.telemetry.Server/webapi/Monitoring/System/SystemMonit.cs
+++ telemetry-server/gRPC.telemetry.Server/webapi/Monitoring/System/SystemMonit.cs
@@ -27,58 +27,66 @@ namespace gRPC.telemetry.Server.webapi.Monitoring.Network
         }
         public override string subscriberUpdateMessage(Subscriber subber)
         {
-            SystemResponse nr = new SystemResponse(systemDeviceInfos.SnapshotTime);
-            foreach(var device in subber.monitoredDevicesIndexes)
+            try
             {
-                if (systemDeviceInfos.MonitoredDevices.TryGetValue(device, out SystemDeviceInfo deviceInfo))
+                SystemResponse nr = new SystemResponse(systemDeviceInfos.SnapshotTime);
+                foreach (var device in subber.monitoredDevicesIndexes)
                 {
-                    nr.monitoredDevices[device] = deviceInfo;
-                }
-                else
-                {
-                    nr.monitoredDevices[device] = null;
-                }
-            }
-
-            for (int i = 0; i < (subber.autoDevicesCount < systemDeviceInfos.MonitoredDevices.Count ? subber.autoDevicesCount: systemDeviceInfos.MonitoredDevices.Count); i++)
-            {
-                nr.autoDevices[systemDeviceInfos.AutoCandidates[i]] = systemDeviceInfos.MonitoredDevices[systemDeviceInfos.AutoCandidates[i]];
-            }
-
-            // TODO: better error selection
-            nr.errors = systemDeviceInfos.SystemErrorsDictionary;
-            int warningsToGet = (systemDeviceInfos.totalWarningsCount > subber.warningCount ? systemDeviceInfos.totalWarningsCount : subber.warningCount);
-
-
-            int count = 0;
-
-            foreach (var kvp in systemDeviceInfos.SystemWarnings)
-            {
-                var warningCount = kvp.Value.warnings?.Count ?? 0;
-
-                if (warningCount == 0)
-                    continue;
-
-                if (count + warningCount <= warningsToGet)
-                {
-                    nr.warnings.TryAdd(kvp.Key, kvp.Value);
-                    count += warningCount;
-                }
-                else
-                {
-                    var partialWarnings = kvp.Value.warnings.Take(warningsToGet - count).ToList();
-
-                    nr.warnings.TryAdd(kvp.Key, new OneDeviceSystemWarningsHolder
+                    if (systemDeviceInfos.MonitoredDevices.TryGetValue(device, out SystemDeviceInfo deviceInfo))
                     {
-                        deviceInfo = kvp.Value.deviceInfo,
-                        warnings = partialWarnings
-                    });
-
-                    break;
+                        nr.monitoredDevices[device] = deviceInfo;
+                    }
+                    else
+                    {
+                        nr.monitoredDevices[device] = null;
+                    }
                 }
-            }
 
-            return JsonSerializer.Serialize(nr, Utils.JsonOption);
+                for (int i = 0; i < (subber.autoDevicesCount < systemDeviceInfos.AutoCandidates.Count ? subber.autoDevicesCount : systemDeviceInfos.AutoCandidates.Count); i++)
+                {
+                    nr.autoDevices[systemDeviceInfos.AutoCandidates[i]] = systemDeviceInfos.MonitoredDevices[systemDeviceInfos.AutoCandidates[i]];
+                }
+
+                // TODO: better error selection
+                nr.errors = systemDeviceInfos.SystemErrorsDictionary;
+                int warningsToGet = (systemDeviceInfos.totalWarningsCount > subber.warningCount ? systemDeviceInfos.totalWarningsCount : subber.warningCount);
+
+
+                int count = 0;
+
+                foreach (var kvp in systemDeviceInfos.SystemWarnings)
+                {
+                    var warningCount = kvp.Value.warnings?.Count ?? 0;
+
+                    if (warningCount == 0)
+                        continue;
+
+                    if (count + warningCount <= warningsToGet)
+                    {
+                        nr.warnings.TryAdd(kvp.Key, kvp.Value);
+                        count += warningCount;
+                    }
+                    else
+                    {
+                        var partialWarnings = kvp.Value.warnings.Take(warningsToGet - count).ToList();
+
+                        nr.warnings.TryAdd(kvp.Key, new OneDeviceSystemWarningsHolder
+                        {
+                            deviceInfo = kvp.Value.deviceInfo,
+                            warnings = partialWarnings
+                        });
+
+                        break;
+                    }
+                }
+
+                return JsonSerializer.Serialize(nr, Utils.JsonOption);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"user: {subber.userID}, autocount: {subber.autoDevicesCount}, error count: {subber.errorCount}");
+                return "";
+            }
         }
         
         public override void onSubscribe(Subscriber subber)
